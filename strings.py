914e6f886efa54e5f0073b1914ce8e8090a80c21
John An <junghooonan@gmail.com>
2025-05-17 23:23:19 -0700

    Add Exercise

---
+++ strings.py
@@ -0,0 +1,10 @@
+# Strings
+## Exercises
+
+# Given the integer num_a complete the function yas 
+# so that it returns the word "Yas!" with that number of "a" letters in it.
+
+# Constraint: 1 <= num_a <= 30
+
+def yas(num_a):
+    return 'Y' + 'a' * num_a + 's!'
